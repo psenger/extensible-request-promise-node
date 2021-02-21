b38d1ce801a747a18ad300c65e4bc2acad3f2bc3
Philip A Senger <philip.a.senger@cngrgroup.com>
2021-02-21 21:07:46 +1100

    ## Added
    * I added more JSDOC to the method... looking for ways to add hooks, I think I might use events.

--- src/index.js
+++ src/index.js
@@ -118,11 +118,32 @@ const request = compose(
 );
 
 module.exports = {
+
     /**
      * HTTP Get with a Path, Options are the NodeJS HttpOptions.
-     * @param {String} url - the base url
+     * @param {String} url - The base url with host schema, name, port, password, base url, etc.
      * @param {Object} [queryParams] - query parameters suitable for `querystring.stringify`
-     * @param {HttpOptions} [httpOptions] - Http Options passed to `http.get`
+     * @param {Object} [httpOptions] - Http Options passed to `http.get`
+     * @param {http.Agent|boolean} [httpOptions.agent] - Controls the agent behavior and can be undefined (default) resulting in the `http.globalAgent` for this `host` and `port` see https://nodejs.org/api/http.html#http_class_http_agent, Agent Object, or false which results in a new Agent with default values.
+     * @param {String} [httpOptions.auth] - Basic authentication i.e. 'user:password' to compute an Authorization header.
+     * @param {Function} [httpOptions.createConnection] - A function that produces a socket/stream to use for the request when the agent option is not used. This can be used to avoid creating a custom Agent class just to override the default createConnection function. See agent.createConnection() for more details. Any Duplex stream is a valid return value.
+     * @param {Number} [httpOptions.defaultPort] - The Default port for the protocol. Default: agent.defaultPort if an Agent is used, else undefined.
+     * @param {Number} [httpOptions.family] - The IP address family to use when resolving host or hostname. Valid values are 4 or 6. When unspecified, both IP v4 and v6 will be used.
+     * @param {Object} [httpOptions.headers] - An object containing request headers.
+     * @param {String} [httpOptions.host] - A domain name or IP address of the server to issue the request to. Default: 'localhost'.
+     * @param {String} [httpOptions.hostname] - Alias for host. To support url.parse(), hostname will be used if both host and hostname are specified.
+     * @param {Boolean} [httpOptions.insecureHTTPParser] - Use an insecure HTTP parser that accepts invalid HTTP headers when true. Using the insecure parser should be avoided. See --insecure-http-parser for more information. Default: false
+     * @param {String} [httpOptions.localAddress] - Local interface to bind for network connections.
+     * @param {Function} [httpOptions.lookup] - Custom lookup function. Default: dns.lookup().
+     * @param {Number} [httpOptions.maxHeaderSize] - Optionally overrides the value of --max-http-header-size for requests received from the server, i.e. the maximum length of response headers in bytes. Default: 16384 (16KB).
+     * @param {String} [httpOptions.method=GET] - A string specifying the HTTP request method. Default: 'GET'.
+     * @param {String} [httpOptions.path=/] - Request path. Should include query string if any. E.G. '/index.html?page=12'. An exception is thrown when the request path contains illegal characters. Currently, only spaces are rejected but that may change in the future. Default: '/'.
+     * @param {Number} [httpOptions.port] - Port of remote server. Default: defaultPort if set, else 80.
+     * @param {String} [httpOptions.protocol=http] - Protocol to use. Default: 'http:'
+     * @param {Boolean} [httpOptions.setHost=true] -Specifies whether or not to automatically add the Host header. Defaults to true.
+     * @param {String} [httpOptions.socketPath] - Unix Domain Socket (cannot be used if one of host or port is specified, those specify a TCP Socket).
+     * @param {Number} [httpOptions.timeout] - A number specifying the socket timeout in milliseconds. This will set the timeout before the socket is connected.
+     * @param {AbortSignal} [httpOptions.signal] - An AbortSignal that may be used to abort an ongoing request.
      * @param {QueryStringOptions} [queryStringOptions] - Http Options passed to `querystring.stringify`
      * @return {Promise<unknown>}
      */
@@ -143,6 +164,14 @@ module.exports = {
         const retries = pick(retryOptions, 'retries');
         return retry(() => request(requestOptions), retries, interval);
     },
+    /**
+     * HTTP Post with data
+     * @param {string} url - the URL
+     * @param body
+     * @param httpOptions
+     * @param retryOptions
+     * @return {Promise<unknown>}
+     */
     post: async function post(url = isRequired('url'), body, httpOptions, retryOptions) {
         const requestOptions = Object.assign(
             {},
